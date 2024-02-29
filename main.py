85ab6b36525a7f2910db804203999855886eacdb
Anish Gharti <anishchengre@kali.chengre>
2024-02-29 18:51:46 +0545

    tablecreated

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-
-
+from db import models
+from db.database import engine
 app = FastAPI()
 
 
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
+
+
+models.Base.metadata.create_all(engine)
