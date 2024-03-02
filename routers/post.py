fe2a735882ee2364be889781779eebf7b95e6d18
anishchengre <anishgharti10@gmail.com>
2024-03-02 10:45:28 +0545

    all posts

--- routers/post.py
+++ routers/post.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from db.database import get_db
 from .schemas import PostDisplay, PostBase
 from sqlalchemy.orm.session import Session
+from db import db_post
+from typing import List
 router = APIRouter(prefix='/post', tags=['post'])
 
 image_url_type = ['absolute', 'relative' ]
 
+
 @router.post('', response_model=PostDisplay)
 def create_post( request: PostBase, db: Session = Depends(get_db)):
-    pass
+    if not request.image_url_type in image_url_type:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="it is  unprocessable")
+    return db_post.create_post(db, request)
+
+
+
+@router.get('/all', response_model=List[PostDisplay])
+def get_all_posts( db: Session = Depends(get_db)):
+    all_posts = db_post.get_all_posts(db)
+    return all_posts
