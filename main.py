20a37c1713d9d7bee520b290714e3b230ccb7269
anishchengre <anishgharti10@gmail.com>
2024-03-02 23:29:01 +0545

    completed

--- main.py
+++ main.py
@@ -4,12 +4,32 @@ from db.database import engine
 from fastapi.staticfiles import StaticFiles
 from routers import user, post, comment
 from auth import authentication
+from fastapi.middleware.cors import  CORSMiddleware
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(post.router  )
 app.include_router(authentication.router)
 app.include_router(comment.router)
+
+
+
+origins = [
+    'http://localhost:3000'
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
+
+
+
+
 
 app.mount('/images', StaticFiles(directory='images'), name='images')
 @app.get("/")
